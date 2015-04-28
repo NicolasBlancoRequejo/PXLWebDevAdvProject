16f22b5b4ae8bff948192a39bdef419df6af18b3
RafaelBelTu <Rafael.Tureluren@STUDENT.PXL.BE>
2015-04-28 15:56:17 +0200

    hopelijk werkt het

--- TEDXPXL/application/views/site_footer.php
+++ TEDXPXL/application/views/site_footer.php
@@ -1,8 +1,20 @@
  
-    <div id="footer">
-        <p>Copywright (c) 2015 raf</p>
+      <div id="footer">
+        <p>Copywright (c) 2015 tedxpxl</p>
     </div>
-        
+    <!-- All Javascript at the bottom of the page for faster page loading -->
+		
+	<!-- First try for the online version of jQuery-->
+	<script src="http://code.jquery.com/jquery.js"></script>
+	
+	<!-- If no online access, fallback to our hardcoded version of jQuery -->
+	<script>window.jQuery || document.write('<script src="includes/js/jquery-1.8.2.min.js"><\/script>')</script>
+	
+	<!-- Bootstrap JS -->
+	<script src="<?php echo base_url(); ?>scripts/bootstrap/bootstrap.min.js"></script>
+	
+	<!-- Custom JS -->
+	<script src="<?php echo base_url(); ?>scripts/script.js"></script>
 	
 </div>
 
