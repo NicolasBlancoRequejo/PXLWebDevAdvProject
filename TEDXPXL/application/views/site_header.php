2d905370df3c0b2273e0bdda89a39e5e2a0b0d13
RafaelBelTu <Rafael.Tureluren@STUDENT.PXL.BE>
2015-05-04 15:38:32 +0200

    buh

---
+++ TEDXPXL/application/views/site_header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title><?php echo $title; ?></title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+        
+        <link href="<?php echo base_url(); ?>styles/bootstrap/bootstrap.min.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>styles/bootstrap/bootstrap-glyphicons.css" rel="stylesheet">        
+
+        <link rel="stylesheet" href="<?php echo base_url(); ?>styles/style.css">
+        
+    </head>
---
+++ TEDXPXL/application/views/site_nav.php
@@ -0,0 +1,33 @@
+<body>	
+    <div class="container" id="main">
+        <div class="navbar navbar-fixed-top">
+            <div class="container">
+                <button class="navbar-toggle" data-target=".navbar-responsive-collapse" data-toggle="collapse" type="button">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand" href="/"><img src="<?php echo base_url(); ?>images/logo.png" alt="Your Logo"></a>
+
+                <div class="nav-collapse collapse navbar-responsive-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">News</a></li>                           
+                        <li><a href="#">TEDXPXL</a></li>                           
+                        <li><a href="#">About</a></li>    
+                        <li><a href="#">Contact</a></li>                           
+
+
+                    </ul>
+
+                    <form class="navbar-form pull-left">
+                        <input type="text" class="form-control" placeholder="Search this site..." id="searchInput">
+                        <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+                    </form>
+
+                </div>
+
+            </div>
+            
+        </div>
