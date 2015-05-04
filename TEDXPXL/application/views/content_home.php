c3cd5ea4e943bca1ca8cd0ac91007487ec0a6283
RafaelBelTu <Rafael.Tureluren@STUDENT.PXL.BE>
2015-05-04 15:35:19 +0200

    startpagina afgewerkt

--- TEDXPXL/application/views/content_home.php
+++ TEDXPXL/application/views/content_home.php
@@ -1,3 +1,15 @@
-<div id="content">
-   
+<div id="banner">
+    <div class="thumbnails row">
+        <div class="thumbnail">
+            <img src="images/carousel_large_01.jpg" alt="PSD to HTML5 & CSS3">
+                <div class="caption">
+                        <h2>We zijn steeds op zoek naar nieuwe leden!</h2>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                        <p><a href="#" class="btn btn-primary btn-neutral" target="_blank">Lid Worden</a></p>
+                </div>
+        </div>
+
+    </div>
 </div>
--- TEDXPXL/application/views/site_footer.php
+++ TEDXPXL/application/views/site_footer.php
@@ -1,22 +1,45 @@
- 
-      <div id="footer">
-        <p>Copywright (c) 2015 tedxpxl</p>
-    </div>
-    <!-- All Javascript at the bottom of the page for faster page loading -->
-		
-	<!-- First try for the online version of jQuery-->
+ </div>
+
+        <footer>
+                <div class="container">
+                        <div class="row">
+                                <div class="col-sm-8">
+                                        <h6>About Us</h6>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                </div>
+
+                                <div class="col-sm-2">
+                                        <h6>Navigation</h6>
+                                        <ul class="unstyled">
+                                            <li><a href="#">Home</a></li>
+                                            <li><a href="#">News</a></li>                           
+                                            <li><a href="#">TEDXPXL</a></li>                           
+                                            <li><a href="#">About</a></li>    
+                                            <li><a href="#">Contact</a></li> 
+                                        </ul>
+                                </div>
+
+                                <div class="col-sm-2">
+                                        <h6>Volg Ons</h6>
+                                        <ul class="unstyled">
+                                                <li><a href="#">Twitter</a></li>
+                                                <li><a href="#">Facebook</a></li>
+                                                <li><a href="#">Google Plus</a></li>
+                                        </ul>
+                                </div>
+                        </div>
+                </div>
+        </footer>
+
+  
 	<script src="http://code.jquery.com/jquery.js"></script>
 	
-	<!-- If no online access, fallback to our hardcoded version of jQuery -->
 	<script>window.jQuery || document.write('<script src="includes/js/jquery-1.8.2.min.js"><\/script>')</script>
 	
-	<!-- Bootstrap JS -->
 	<script src="<?php echo base_url(); ?>scripts/bootstrap/bootstrap.min.js"></script>
 	
-	<!-- Custom JS -->
 	<script src="<?php echo base_url(); ?>scripts/script.js"></script>
 	
-</div>
 
 </body>
 </html>
