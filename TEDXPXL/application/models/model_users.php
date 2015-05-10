d4983b78b2b3abde2305d9a65eb013258df7cd85
Jasper05 <Jasper.Bas@student.pxl.be>
2015-05-10 15:41:00 +0200

    kalender toegevoegd met het script voor de database te maken

--- TEDXPXL/application/models/model_users.php
+++ TEDXPXL/application/models/model_users.php
@@ -3,7 +3,7 @@
 	class Model_users extends CI_Model {
 		public function can_log_in(){
 			$this->db->where('email', $this->input->post('email'));
-			$this->db->where('password', md5(($this->input->post('password')));
+			$this->db->where('password', md5(($this->input->post('password'))));
 			
 			$query = $this->db->get('users');
 			
