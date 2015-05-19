933debca77734f6b6455a033adbe94495fb14f1e
nicolasblancorequejo <nicolas.blancorequejo@gmail.com>
2015-05-19 15:14:25 +0200

    Contact pagina gemaakt, links gefixt, lorem ipsum vervangen, niet gebruikte functies gecomment

---
+++ TEDXPXL.be/application/views/contact/contact.php
@@ -0,0 +1,18 @@
+<div id="banner">
+    <div class="thumbnails row">
+        <div class="thumbnail">
+            <div class="caption">
+                <h2 style="color: #FF2B06">Contact Information</h2>
+                <a><img src="<?php echo base_url('images/pxllogo.png"'); ?>" alt="PXL Logo"></a>
+                <p>Hogeschool PXL</p>
+                <p>Elfde-Liniestraat 24</p>
+                <p>B-3500 Hasselt</p>
+                <p>tel. + 32 11 77 55 55</p>
+                <p>fax. + 32 11 77 55 59</p>
+                <p><a href="http://www.pxl.be">www.pxl.be</a></p>
+                <p><a href="mailto:pxl@pxl.be">pxl@pxl.be</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
